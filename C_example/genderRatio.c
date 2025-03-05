bbda23a42fa0f82c0b094de429a37b3f8ca36a52
PIRI <kdp9477@gmail.com>
2025-03-05 12:26:11 +0900

    add genderRatio, int long overflow

---
+++ C_example/genderRatio.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int man, woman;
+    double sum, manRate, womanRate;
+
+    // 입력받는 코드
+    printf("남자 인원 : ");
+    scanf("%d", &man);
+    printf("여자 인원 : ");
+    scanf("%d",&woman);
+
+    // 연산하는 코드
+    sum = man + woman;
+    manRate = (double)man / sum * 100;
+    womanRate = (double)woman / sum * 100;
+
+    printf("남자의 수는 %d명이고 여자의 수는 %d명이다.\n", man, woman);
+    printf("총 수는 %.f명\n남자의 비율은 %.2f%%\n여자의 비율은 %.2f%%\n", sum, manRate, womanRate);
+
+    return 0;
+}
--- C_example/limit.c
+++ C_example/limit.c
@@ -11,6 +11,12 @@ int main(void)
     printf("char_bit : %d\n", CHAR_BIT);
     printf("char_max : %d\n", CHAR_MAX);
     
+    printf("INT_MIN : %d \t INT_MAX : %d\n", INT_MIN, INT_MAX);
+    printf("LONG_MIN : %ld \t LONG_MAX : %ld\n", LONG_MIN, LONG_MAX);
+    
+    printf("INT_MIN : %d \t INT_MAX + 1 : %d\n", INT_MIN, INT_MAX + 1);
+    printf("LONG_MIN : %ld \t LONG_MAX + 1 : %ld\n", LONG_MIN, LONG_MAX + 1);
+
     return 0;
 }
 
