4d6b1b22e489f1b9fddd00b61c26caa073b52da4
PIRI <kdp9477@gmail.com>
2025-05-15 16:53:03 +0900

    add python asyncio_http, uv_pygame

---
+++ python/gravity_pygame/src/main.py
@@ -0,0 +1,16 @@
+# pip install uv
+# uv init
+# uv add pygame
+# uv run src/main.py 
+
+from gravity import Game
+
+
+def main():
+    print("안녕하세요! pygame main 입니다!")
+    game = Game()
+    game.run()
+
+
+if __name__ == "__main__":
+    main()
